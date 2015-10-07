c2fba8b9d4668da0adc1a89ac25c0508e9b3ec0a
Jonathan Howard <j@hovverd.com>
2015-10-07 00:04:38 -0400

    added initial version of entity

---
+++ src/physics/entity.h
@@ -0,0 +1,120 @@
+/* 
+ * Copyright (c) 2015 Jonathan Howard
+ * License: https://github.com/v3n/altertum/blob/master/LICENSE
+ */
+
+#pragma once
+
+#include "math/math_types.h"
+#include "math/vector3.h"
+
+using namespace altertum;
+
+struct BoundingSphere
+{
+    Vector3 origin;
+    float radius;
+
+    inline bool check_collision(BoundingSphere& s2)
+    {
+        BoundingSphere s1 = *this;
+
+        Vector3 pos = s1.origin - s2.origin;
+        float distance_sq = pos.x * pos.x + pos.y * pos.y;
+        float min_dist_sq = s1.radius + s2.radius;
+
+        return distance_sq <= (min_dist_sq * min_dist_sq);
+    }
+
+    inline void update(Vector3& v)
+    {
+        origin += v; 
+    }
+};
+
+struct PhysicsBody
+{
+    /** collision body radius */
+    BoundingSphere collision;
+
+    /** 2D space */
+    float angle;
+    
+    float lastAngle;
+    float torque;
+    float speed;
+    float mass;
+
+    float angularVelocity;
+    float angularSpeed;
+
+    /** 3D space */
+    Vector3 position;
+    Vector3 lastPosition;
+    Vector3 force;
+    Vector3 velocity;
+    Vector3 acceleration;
+    Vector3 impulse;
+
+    inline PhysicsBody init_body(   Vector3& pos,
+                                    float _mass, 
+                                    float angle, 
+                                    float radius,
+                                    float length
+                                )
+    {
+        position     = pos;
+        lastPosition = pos;
+        
+        collision.origin = pos;
+        collision.radius = radius;
+
+        mass         = _mass;
+
+        angle        = angle;
+        lastAngle    = angle;
+
+        speed        = 0.0f;
+
+        force        = vector3::vector3( 0.0f, 0.0f, 0.0f );
+        velocity     = vector3::vector3( 0.0f, 0.0f, 0.0f );
+        acceleration = vector3::vector3( 0.0f, 0.0f, 0.0f );
+        impulse      = vector3::vector3( 0.0f, 0.0f, 0.0f );
+    }
+
+    inline void applyGravity()
+    {
+        force.y += mass * 0.001f;
+    }
+
+    /**
+     * update body
+     * @param deltaTime  time difference
+     * @param correction deltaTime / lastDeltaTime
+     */
+    inline void update(float deltaTime, float correction)
+    {
+        float deltaTimeSq = pow(deltaTime, 2);
+
+        /* compute last step values */
+        float frictionAir = 1 - frictionAir;
+        Vector3 lastVelocity = position - lastPosition;
+
+        /* Verlet integration for velocity */
+        velocity = (lastVelocity * frictionAir * correction) + (force / mass) * deltaTimeSq;
+        lastPosition = position;
+        position += velocity;
+
+        /* Verlet integration for angular velocity */
+        angularVelocity = ((angle - lastAngle) * frictionAir * correction);
+        lastAngle = angle;
+        angle += angularVelocity;
+
+        /* track speed and acceleration */
+        speed = vector3::distance(velocity);
+        angularSpeed = abs(angularVelocity);
+
+        /* update bounds */
+        collision.update(velocity);
+    }
+};
